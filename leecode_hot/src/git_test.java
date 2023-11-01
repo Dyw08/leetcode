8d5d559a9576d846c9da0b2f15fff800a4254ec2
yingwei <yingwei@itcast.cn>
2023-11-01 10:30:41 +0800

    master commit1

--- leecode_hot/src/git_test.java
+++ leecode_hot/src/git_test.java
@@ -2,6 +2,7 @@ public class git_test {
     public static void main(String[] args){
         System.out.println("hello,git");
         System.out.println("hello,git4");
+        System.out.println("master");
     }
 
 }
