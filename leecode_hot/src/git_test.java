019b9017118eddecad1fced8d87d4ba4f750c831
yingwei <yingwei@itcast.cn>
2023-11-01 10:27:38 +0800

    hot-fix commit1

--- leecode_hot/src/git_test.java
+++ leecode_hot/src/git_test.java
@@ -1,8 +1,7 @@
 public class git_test {
     public static void main(String[] args){
         System.out.println("hello,git");
-        System.out.println("hello,git2");
-        System.out.println("hello,git3");
+        System.out.println("hello,git4");
     }
 
 }
